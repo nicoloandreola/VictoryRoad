dacc992e1d441c8a5efaefe17e863cd74cae1296
Nicolò Andreola <nicolo.andreola@studenti.unicam.it>
2026-08-26 17:57:25 +0200

    [UPDATE-MODEL] "Aggiunta annotation @FunctionalInterface a ModificatoreStatistiche"

--- app/src/main/java/it/unicam/cs/mpgc/rpg129542/model/stats/ModificatoreStatistiche.java
+++ app/src/main/java/it/unicam/cs/mpgc/rpg129542/model/stats/ModificatoreStatistiche.java
@@ -6,12 +6,17 @@ import lombok.NonNull;
  * Interfaccia che definisce il contratto da rispettare per l'implementazione
  * di un oggetto che modifica le statistiche di base TEMPORANEAMENTE durante un match.
  *
- * Le classi che la implementano dovranno sovrascrive un solo metodo che permette
- * appunto di applicare variazioni ai valori delle statistiche di base.
+ * Le classi che la implementano dovranno sovrascrive un solo metodo che descrive una
+ * trasformazione da un'istanza di {@link StatisticheBase} a una nuova istanza contenente i valori modificati.
+ *
+ * L'annotazione {@link FunctionalInterface} permette al compilatore di verificare che
+ * l'interfaccia mantenga un unico metodo astratto e consente inoltre, quando opportuno,
+ * di fornire implementazioni anche tramite espressioni lambda o method reference, oltre
+ * che attraverso classi concrete come {@link BonusStatistiche} e {@link MalusStatistiche}.
  *
  * @author Nicolò Andreola
  */
-
+@FunctionalInterface
 public interface ModificatoreStatistiche {
 
     /**
